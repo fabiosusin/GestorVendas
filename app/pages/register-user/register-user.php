a1bfb94a8203ebe4f5c0163f4fcf3356ebcd8da0
fabiosusin <fabiosusinoberherr@gmail.com>
2021-04-13 23:12:46 -0300

    init

---
+++ app/pages/register-user/register-user.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8" />
+  <title>Login</title>
+  <link rel="stylesheet" href="login.css">
+  <link rel="stylesheet" href="/GestorVendas/app/pages/shared/css/variables.css">
+  <link rel="stylesheet" href="/GestorVendas/app/pages/shared/css/layout.css">
+  <script src="https://kit.fontawesome.com/9550dd9c94.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+  <div class="login-page">
+    <form>
+      <div style="background-image: url(/GestorVendas/app/assets/images/svg/icons8-shopify.svg);"></div>
+      <div class="input-with-icon">
+        <i class="fas fa-envelope"></i>
+        <input placeholder="Email" />
+      </div>
+      <div class="input-with-icon">
+        <i class="fas fa-lock"></i>
+        <input placeholder="Senha" />
+      </div>
+      <a>Esqueci minha senha</a>
+      <button>ENTRAR</button>
+
+      <p>Não tem conta? <a>Então Registre-se</a></p>
+    </form>
+  </div>
+</body>
+
+</html> 
