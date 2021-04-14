a5022910fd29a8e822e5bb956187fd1566ae354f
William Felipe da Silva <wfsilva2@ucs.br>
2021-04-14 14:20:09 -0300

    Update BD.sql

---
+++ SCRIPT/BD.sql
@@ -0,0 +1,76 @@
+CREATE TABLE Fornecedor (
+    id int NOT NULL AUTO_INCREMENT,
+    nome varchar(255) NOT NULL,
+    descricao varchar(255),
+    telefone varchar(20),
+	email varchar(100),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Cliente (
+    id int NOT NULL AUTO_INCREMENT,
+    nome varchar(255) NOT NULL,
+    telefone varchar(20),
+	email varchar(100),
+	cartaoCredito varchar(100),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Endereco (
+    id int NOT NULL AUTO_INCREMENT,
+    rua varchar(255) NOT NULL,
+    numero char(5),
+    complemento varchar(255),
+	bairro varchar(100),
+	cep varchar(20),
+	cidade varchar(100),
+	estado char(2),
+	ClienteID int NOT NULL,
+	FornecedorID int NOT NULL,
+	PRIMARY KEY (id),
+    FOREIGN KEY (ClienteID) REFERENCES Cliente(id),
+	FOREIGN KEY (FornecedorID) REFERENCES Fornecedor(id)	
+);
+
+CREATE TABLE Produto (
+    id int NOT NULL AUTO_INCREMENT,
+    nome varchar(255) NOT NULL,
+	descricao varchar(255) NOT NULL,
+    foto blob,
+	FornecedorID int NOT NULL,
+	FOREIGN KEY (FornecedorID) REFERENCES Fornecedor(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Estoque (
+    id int NOT NULL AUTO_INCREMENT,
+    quantidade int NOT NULL,
+	preco double NOT NULL,
+	ProdutoID int NOT NULL,
+	FOREIGN KEY (ProdutoID) REFERENCES Produto(id),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Pedido (
+    id int NOT NULL AUTO_INCREMENT,
+	numero int NOT NULL,
+    dataPedido date NOT NULL,
+	dataEntrega date NOT NULL,
+	situacao VARCHAR(10) NOT NULL,
+	ClienteID int NOT NULL, 
+	ProdutoID int NOT NULL,
+	FOREIGN KEY (ClienteID) REFERENCES Cliente(id),
+	FOREIGN KEY (ProdutoID) REFERENCES Produto(id),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE ItemPedido (
+    id int NOT NULL AUTO_INCREMENT,
+	quantidade int NOT NULL,
+    preco double NOT NULL,
+	PedidoID int NOT NULL, 
+	ProdutoID int NOT NULL,
+	FOREIGN KEY (PedidoID) REFERENCES Pedido(id),
+	FOREIGN KEY (ProdutoID) REFERENCES Produto(id),
+	PRIMARY KEY (id)
+);
